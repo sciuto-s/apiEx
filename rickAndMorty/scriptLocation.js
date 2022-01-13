aa49c9058aa57adec087e3e945bba08e717b0b4c
Simone Sciuto <simoSciuto@MacBook-Pro-di-Simone.local>
2022-01-13 11:08:53 +0100

    v1.1

--- rickAndMorty/scriptLocation.js
+++ rickAndMorty/scriptLocation.js
@@ -1,21 +1,33 @@
 const locations = document.querySelector('.containerLocation');
-const next3 =document.querySelector('.next2');
+const next3 = document.querySelector('.next2');
 const prev3 = document.querySelector('.prev2')
-let pageLo=1;
+const results = document.querySelector('.results');
+let pageLo = 1;
 const getLocation = function () {
     fetch(`https://rickandmortyapi.com/api/location/?page=${pageLo}`)
         .then(resp => resp.json())
         .then(data => {
-            console.log(data);
-         
-            }      
-        )}
+            results.innerHTML=''
+            for (const location of data.results) {
+             results.innerHTML += `
+             <tr>
+                <th scope="row">${location.name}</th>
+                <td data-title="type">${location.type}</td>
+                <td data-title="dimension">${location.dimension}</td>
+                <td data-title="residents"><a href="./resident.html">residents</a></td>
+               
+              </tr>
+             `
+            }
+        }
+        )
+}
 getLocation()
 next3.addEventListener('click', () => {
     pageLo++
-   getLocation()
+    getLocation()
 })
 prev3.addEventListener('click', () => {
-   pageLo--
-  getLocation()
+    pageLo--
+    getLocation()
 })
